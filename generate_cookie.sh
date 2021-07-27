6eafd3f9058d8f2ca896c0abbeeb66a2a8af4119
saurabhkothari <saurabhkothari@rocketmail.com>
2021-07-27 22:23:11 +0530

    Create generate_cookie.sh

---
+++ generate_cookie.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+APIC="10.76.101.138"
+
+printf "\n### logging in ###\n"
+
+OUT=$(curl -s -X POST -k https://$APIC/api/aaaLogin.json -d '{ "aaaUser" : { "attributes" : { "name" : "admin" , "pwd" : "cisco'\!'1234" } } }' -c cookie.txt EOF)
+ERRCODE=$(echo $OUT | jq --raw-output '.imdata[0] .error .attributes .code')
+ERRTEXT=$(echo $OUT | jq --raw-output '.imdata[0] .error .attributes .text')
+
+# Check if an error code exists
+if [ "${ERRCODE//[0-9]}" = "" ]
+then
+	printf "$ERRCODE : $ERRTEXT\n"
+	exit 0
+else
+	printf "Login OK"
+fi
