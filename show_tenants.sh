1d92dda9f4d47f9466f7ada8cb2e93295b11081b
saurabhkothari <saurabhkothari@rocketmail.com>
2021-07-27 23:13:03 +0530

    Update show_tenants.sh

--- show_tenants.sh
+++ show_tenants.sh
@@ -2,8 +2,6 @@
 
 APIC="10.76.101.138"
 
-printf "\n### logging in ###\n"
-
 OUT=$(curl -s -X POST -k https://$APIC/api/aaaLogin.json -d '{ "aaaUser" : { "attributes" : { "name" : "admin" , "pwd" : "cisco'\!'1234" } } }' -c cookie.txt EOF)
 ERRCODE=$(echo $OUT | jq --raw-output '.imdata[0] .error .attributes .code')
 ERRTEXT=$(echo $OUT | jq --raw-output '.imdata[0] .error .attributes .text')
@@ -17,5 +15,5 @@ else
 	printf "Login OK"
 fi
 
-printf "\n ### Printing list of all Tenants ###\n\n"
+printf "\n\n ### Printing list of all Tenants ###\n\n"
 curl -s -b cookie.txt -X GET -k https://$APIC/api/node/class/fvTenant.json | jq --raw-output '.imdata[] .fvTenant .attributes .name'
