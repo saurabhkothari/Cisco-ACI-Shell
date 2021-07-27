f56338e90f163bafeeb9086d2d01778fc21ba4d1
saurabhkothari <saurabhkothari@rocketmail.com>
2021-07-27 23:13:23 +0530

    Update delete_tenant.sh

--- delete_tenant.sh
+++ delete_tenant.sh
@@ -16,5 +16,5 @@ then
 else
 	printf "Login OK"
 fi
-printf "\n ### Deleting Tenant ###\n\n"
+printf "\n\n ### Deleting Tenant ###\n\n"
 curl -s -b cookie.txt -X POST -k https://$APIC/api/mo/uni.json -d '{"fvTenant":{"attributes":{"name":"Jenkins123", "status":"deleted"}}}'
